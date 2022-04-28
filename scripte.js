1be3c2e6fe56c63defd3bcc27e34cbfa7e1622a8
EminEhmedli <ekoemin91@gmail.com>
2022-04-28 19:08:25 +0400

    first commit

--- scripte.js
+++ scripte.js
@@ -1,18 +1,19 @@
 
 let seki = document.querySelector("#seki");
 let toplar = document.querySelector('#top');
-// let xal = document.querySelector('#xal');
+let xal = document.querySelector('#xal');
 let sekiX = 0;
 let topX = 0;
 let topY = 0;
-let dx = 5;
+let dx = 6;
 let dy = 5;
+let point=0
 
 
 
 let set =  setInterval(move,40)
 onkeydown = barMove;
-// xal.innerHTML = `Sizin Xal: ${point}`;
+xal.innerHTML = `Sizin Xal: ${point}`;
 
 
 
@@ -42,12 +43,13 @@ function move() {
         dy *= -1;
     }
 
-    else if (topY > 560 && topX >= sekiX - 25 && topX <= sekiX + 100) {
+    else if (topY > 560 && topX >= sekiX - 35 && topX <= sekiX + 120) {
         // dy++;
         // dx++;
         dy *= -1;
-        // point++;
-        // xal.innerHTML = `Sizin Xal: ${point}`;
+        point++;
+        xal.innerHTML = `Sizin Xal: ${point}`;
+        console.log(point);
         // ball.style.backgroundColor = rndColor();
         // bar.style.backgroundColor = rndColor();
         // document.getElementById('general').style.borderColor = rndColor();
@@ -55,13 +57,22 @@ function move() {
 
 
     else if (topY > 560) {
-        document.body.innerHTML = "Uduzdunuz";
+        document.body.innerHTML =`Uduzdunuz : <br/> Sizin Neticeniz ${point} bal <br/>
+        Yeniden oyuna baslamaq isteyirsiniz? <button onclick="Begin()"> Basla </button>`;
+
         clearInterval(set);
     }
+    
 
 
     topX += dx;
     topY += dy;
     toplar.style.left = topX + 'px';
     toplar.style.top = topY + 'px';
+}
+function Begin(){
+   
+    let set =  setInterval(move,40)
+onkeydown = barMove;
+xal.innerHTML = `Sizin Xal: ${point}`;
 }
